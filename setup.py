ee370f73c59b3f2c5c887d94269c9142f6a633eb
Jacopo <dedalusj@gmail.com>
2018-11-09 20:58:21 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import io
+import re
+from setuptools import setup
+
+with io.open("click/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
+
+setup(
+    name="ssm-config",
+    version=version,
+    license="MIT",
+    author="Jacopo Sabbatini",
+    author_email="dedalusj@gmail.com",
+    description="Python package to load environment configuration from YAML files "
+                "supporting overriding from environment variables and AWS SSM",
+    packages=["ssm_config"],
+    package_dir={'': 'src'},
+    python_requires=">=3.4",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
--- src/ssm_config/__init__.py
+++ src/ssm_config/__init__.py
@@ -1,3 +1,5 @@
 from .config import load_env
 
 __all__ = ['load_env']
+
+__version__ = '0.0.1'
